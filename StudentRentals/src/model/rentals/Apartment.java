6058a445012e8ba6dd9c718c036f7c3ad452e056
Daayim Mustapha <mustaphad@cardiff.ac.uk>
2026-01-05 10:58:49 +0000

    added attributes and setter&getter methods to subclass of rental

--- StudentRentals/src/model/rentals/Apartment.java
+++ StudentRentals/src/model/rentals/Apartment.java
@@ -1,4 +1,21 @@
 package model.rentals;
 
 public class Apartment extends Rental{
+
+    private String typeOfApartment;
+
+    public Apartment(String typeOfApartment) {
+        this.typeOfApartment = typeOfApartment;
+    }
+
+    public Apartment() {
+    }
+
+    public String getTypeOfApartment() {
+        return typeOfApartment;
+    }
+
+    public void setTypeOfApartment(String typeOfApartment) {
+        this.typeOfApartment = typeOfApartment;
+    }
 }
--- StudentRentals/src/model/rentals/House.java
+++ StudentRentals/src/model/rentals/House.java
@@ -1,5 +1,20 @@
 package model.rentals;
 
 public class House extends Rental{
+    private double area;
 
+    public House(double area) {
+        this.area = area;
+    }
+
+    public House() {
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
 }
